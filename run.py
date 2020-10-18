8d55d33664c5196d72269c03c1fd6d7184ac933f
AndroShivam <52043419+AndroShivam@users.noreply.github.com>
2020-10-18 17:48:53 +0530

    some minor fixes

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import sys
-import main
+import RedditWallChanger
 
 subreddit_name = sys.argv[1]
 
 print(f"Setting wallpaper from r/{subreddit_name}...")
 
-wallpaper = main.set_wallpaper(subreddit_name = subreddit_name)
+wallpaper = RedditWallChanger.set_wallpaper(subreddit_name = subreddit_name)
 
 print("Done!")
 
